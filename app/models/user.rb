c7ccf78012258743003f7c328c1a5d8bfff14e1d
kiyotaka tomii <kiyotaka.tomii@gmail.com>
2018-04-08 13:18:34 +0000

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -14,6 +15,28 @@ class User < ApplicationRecord
                             BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+  
+  #return random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  #memorize user to db for permanent session
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
 
+  #return true if passed token coincide with digest
+  def authenticated?(remenber_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  #delete user login information
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+  
 end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,15 +3,32 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper
   
+  
   #return true when test user logined
   def is_logged_in?
     !session[:user_id].nil?
   end
-
+  
+  #log in as a test user
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
+  
+  #log in as a test user
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                      password: password,
+                      remember_me: remember_me } }
+  end
+  
 end
