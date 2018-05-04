3e02d338be531188482d066c7f80fa9efb8b6590
kiyotaka tomii <kiyotaka.tomii@gmail.com>
2018-05-04 06:32:15 +0000

    revise spell

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = '<your heroku app>.herokuapp.com'
-  config.action_mailer.default_url_option = { host: host }
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
@@ -76,6 +76,7 @@ Rails.application.configure do
     :password => ENV['SENDING_PASSWORD'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
+    
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
