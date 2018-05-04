aa182f22cabf86a167b2035beb7492ddc0d178fc
kiyotaka tomii <kiyotaka.tomii@gmail.com>
2018-05-04 08:28:31 +0000

    debug

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!(name: "Example User",
-            email: "example180504@railstutorial.org",
+            email: "example@railstutorial.org",
             password: "foobar",
             password_confirmation: "foobar",
             admin: true,
