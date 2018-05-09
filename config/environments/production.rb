2e663903c88d3e3a3f796efb86ef3f85512fb245
kiyotaka tomii <kiyotaka.tomii@gmail.com>
2018-05-09 20:27:01 +0000

    revise smtp setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
     :port => '587',
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDING_PASSWORD'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
     
