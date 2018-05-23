6e03dacdcbda93db15f8f25df52534c0355a4871
kiyotaka tomii <kiyotaka.tomii@gmail.com>
2018-05-23 21:30:41 +0000

    revise feed CSS

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,6 +1,6 @@
 class RelationshipsController < ApplicationController
   before_action :logged_in_user
-  
+
   def create
     @user = User.find(params[:followed_id])
     current_user.follow(@user)
@@ -9,7 +9,7 @@ class RelationshipsController < ApplicationController
       format.js
     end
   end
-  
+
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow(@user)
@@ -18,6 +18,4 @@ class RelationshipsController < ApplicationController
       format.js
     end
   end
-  
-  
 end
